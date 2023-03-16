9c3475b6a1f80be08b9415eb192c85ad7975e414
mrKappa27 <borghi.kevin@gmail.com>
2023-03-16 23:27:47 +0100

    week4: db scripts,  conn pooling, act fetch

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,12 +1,12 @@
 INSERT INTO public.users (display_name, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' ,'MOCK'),
+  ('KevvoRT', 'kevvort' ,'78919c1f-d161-46e3-874d-cbe9facfc915'),
   ('Andrew Bayko', 'bayko' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'kevvort' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
