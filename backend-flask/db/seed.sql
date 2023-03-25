576030d9eb5a5d18ea0a30dc57ccfcd4a164b707
mrKappa27 <borghi.kevin@gmail.com>
2023-03-25 17:13:47 +0100

    week5: update seed with new uuid

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,6 +1,6 @@
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('KevvoRT', 'kevvort@hotmail.it', 'kevvort', '78919c1f-d161-46e3-874d-cbe9facfc915'),
+  ('KevvoRT', 'kevvort@hotmail.it', 'kevvort', '3e6f3e3c-23ce-4929-a08a-0145d276de6c'),
   ('Andrew Bayko', 'bayko@exampro.co', 'bayko', 'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
