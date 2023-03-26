7cbc34d21ab5d592c6cb4946a59edd59ce0edbee
mrKappa27 <borghi.kevin@gmail.com>
2023-03-26 21:16:13 +0200

    week5: add MessageGroupNewPage

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -8,6 +8,7 @@ import SigninPage from './pages/SigninPage';
 import RecoverPage from './pages/RecoverPage';
 import MessageGroupsPage from './pages/MessageGroupsPage';
 import MessageGroupPage from './pages/MessageGroupPage';
+import MessageGroupNewPage from './pages/MessageGroupNewPage';
 import ConfirmationPage from './pages/ConfirmationPage';
 import React from 'react';
 import {
@@ -52,6 +53,10 @@ const router = createBrowserRouter([
   {
     path: "/messages/:message_group_uuid",
     element: <MessageGroupPage />
+  },
+  {
+    path: "/messages/new/:handle",
+    element: <MessageGroupNewPage />
   },
   {
     path: "/signup",
