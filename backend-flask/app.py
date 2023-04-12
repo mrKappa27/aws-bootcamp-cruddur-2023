a764625163c32efd7e224d798bbe839d8e19df70
mrKappa27 <borghi.kevin@gmail.com>
2023-04-12 22:34:33 +0200

    week8: week8 live

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -106,7 +106,7 @@ cors = CORS(
 @app.after_request
 def after_request(response):
     timestamp = strftime('[%Y-%b-%d %H:%M]')
-    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+    #LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
     return response
 
 @app.route('/api/health-check')
---
+++ thumbing-serverless-cdk/jest.config.js
@@ -0,0 +1,10 @@
+"use strict";
+module.exports = {
+    testEnvironment: 'node',
+    roots: ['<rootDir>/test'],
+    testMatch: ['**/*.test.ts'],
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest'
+    }
+};
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiamVzdC5jb25maWcuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJqZXN0LmNvbmZpZy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUEsTUFBTSxDQUFDLE9BQU8sR0FBRztJQUNiLGVBQWUsRUFBRSxNQUFNO0lBQ3ZCLEtBQUssRUFBRSxDQUFDLGdCQUFnQixDQUFDO0lBQ3pCLFNBQVMsRUFBRSxDQUFDLGNBQWMsQ0FBQztJQUMzQixTQUFTLEVBQUU7UUFDVCxhQUFhLEVBQUUsU0FBUztLQUN6QjtDQUNGLENBQUMiLCJzb3VyY2VzQ29udGVudCI6WyJtb2R1bGUuZXhwb3J0cyA9IHtcclxuICAgIHRlc3RFbnZpcm9ubWVudDogJ25vZGUnLFxyXG4gICAgcm9vdHM6IFsnPHJvb3REaXI+L3Rlc3QnXSxcclxuICAgIHRlc3RNYXRjaDogWycqKi8qLnRlc3QudHMnXSxcclxuICAgIHRyYW5zZm9ybToge1xyXG4gICAgICAnXi4rXFxcXC50c3g/JCc6ICd0cy1qZXN0J1xyXG4gICAgfVxyXG4gIH07Il19
